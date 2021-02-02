d30ddd1440f60fb528faec2ee1ac5f3d7777418e
Kopolic <45522094+Kopolic@users.noreply.github.com>
2021-02-02 18:11:19 +0300

    master

--- src/main/java/ru/sapteh/Program.java
+++ src/main/java/ru/sapteh/Program.java
@@ -7,13 +7,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class Program extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/clients.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("ru/sapteh/clients.fxml")));
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Бд");
         primaryStage.setResizable(false);
